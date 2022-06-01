573c57f5936da6ed4a8fb7c1aea2638be4e562de
falkowa42 <falkowa42@gmail.com>
2022-06-01 07:58:44 -0400

    Initial commit with some additional course changes

---
+++ ng-fundamentals/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { EventsAppComponent } from './events-app.component';
+import { EventListComponent } from './events/event-list.component';
+import { NavBarComponent } from './nav/navbar.component';
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [
+    EventsAppComponent,
+    EventListComponent,
+    EventThumbnailComponent,
+    NavBarComponent,
+  ],
+  bootstrap: [EventsAppComponent],
+})
+export class AppModule {}
---
+++ ng-fundamentals/src/app/events-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: `<app-navbar></app-navbar> <event-list></event-list>`,
+})
+export class EventsAppComponent {
+  title = 'ng-fundamentals';
+}
